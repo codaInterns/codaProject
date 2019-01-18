3e7fc734b687997bfc5ef7c63ccdb5785be96d9a
kaushiks15103 <kaushiksivaram11@gmail.com>
2019-01-18 11:05:54 +0530

    kaushik commit

---
+++ kaushik/src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule} from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material';
+import {HttpClientModule} from '@angular/common/http';
+import {MatInputModule} from '@angular/material';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { UserloginComponent } from './userlogin/userlogin.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+
+const appRoutes: Routes = [
+  {
+    path : '',
+    component : UserloginComponent
+  },
+  {
+    path : 'app-mainpage',
+    component : MainpageComponent
+  }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    UserloginComponent,
+    MainpageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatInputModule,
+    MatTableModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
+    ReactiveFormsModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
